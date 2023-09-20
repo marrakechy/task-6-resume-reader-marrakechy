2d1ece1256b76e78786d8ef3330cca00cc45e9d1
marrakechy <belmarrakechy21@coe.edu>
2023-09-19 20:57:37 -0500

    look for pdf format through resume only

--- Resumes.py
+++ Resumes.py
@@ -3,10 +3,16 @@ import PyPDF2
 
 resume_folder = 'Resumes'
 
-# Get all files from the specified folder
+#get all files from the specified folder
 files = os.listdir(resume_folder)
 
-# Loop through all files
+#loop through all files
 for file in files:
+    #check if the file extension is .pdf
+    if file.endswith('.pdf'):
+        #file path
+        file_path = os.path.join(resume_folder, file)
+
+
 
 
