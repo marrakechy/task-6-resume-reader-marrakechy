134393e01adeb7a01b29ee7c9a52a1262c83ef45
marrakechy <belmarrakechy21@coe.edu>
2023-09-20 21:25:33 -0500

    Completed- grade me please

--- Resumes.py
+++ Resumes.py
@@ -6,13 +6,14 @@ try:
 
         data = infile.read()
         pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        emails = re.findall(pattern, data) #find email addresses
 
-        for p in pdfReader.pages:
-            print(p.extract_text())
+        for e in set(emails):
+            print(e)
 
 except Exception as e:
     # Print any exception that occurs during file reading
-    print(f"Could not read due to: {str(e)}")
+    print(f'Could not read due to: {str(e)}')
 
 
 
