edec2a4c7c9ba64b3081e68e7551d1433da51f03
marrakechy <belmarrakechy21@coe.edu>
2023-09-20 21:03:34 -0500

    grade me- completed

--- Resumes.py
+++ Resumes.py
@@ -2,10 +2,18 @@
 import os
 import PyPDF2
 
-with open("Resumes/app-0009.pdf","rb") as infile:
-    pdfReader = PyPDF2.PdfReader(infile)
-    for p in pdfReader.pages:
-        print(p.extract_text())
+try:
+    with open("Resumes/app-0009.pdf","rb") as infile:
+
+        pdfReader = PyPDF2.PdfReader(infile)
+        for p in pdfReader.pages:
+            print(p.extract_text())
+
+except Exception as e:
+    # Print any exception that occurs during file reading
+    print(f"Could not read due to: {str(e)}")
+
+
 
 
 
